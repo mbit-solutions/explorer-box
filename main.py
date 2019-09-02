02a8aff2c8925fa4cb47660811ea690475104bf2
Daniel Herzog <Daniel.Herzog@mbit.at>
2019-09-02 21:42:55 +0200

    bugfix main inject dependencies

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sandbox as sb
 import kinect as ki
 import config as cfg
+import renderer as rd
 import cv2
 
 def main():
 	cv2.namedWindow('sandbox')
 	
-	config = cfg.Config()	
-	sandbox = sb.Sandbox(config, ki.Kinect())
+	config = cfg.Config()		
+	sandbox = sb.Sandbox(config, ki.Kinect(), rd.Renderer(config))
 	sandbox.execute()
 	
 	cv2.destroyAllWindows()
