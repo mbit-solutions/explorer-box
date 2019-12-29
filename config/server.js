0728ac056ca8582395b534bf47ae114ba210e784
Daniel Herzog <danielherzog@outlook.com>
2019-12-29 22:55:27 +0100

    update node server

--- config/server.js
+++ config/server.js
@@ -11,44 +11,42 @@ var app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/logs', function(req, res) {
-    fs.readFile(logFileName, (err, data) => {        
-        if (err) 
-        {
+app.get('/logs', (req, res) => {
+    fs.readFile(logFileName, (err, data) => {
+        if (err) {
             console.error(err);
             res.end();
-        }  
+        }
         res.send(data);
-    }); 
+    });
 });
 
-app.get('/image', function(req, res){
-    fs.readFile(imageFileName, (err, data) => {        
-        if (err) 
-        {
+app.get('/image', (req, res) => {
+    fs.readFile(imageFileName, (err, data) => {
+        if (err) {
             console.error(err);
             res.end();
-        }    
+        }
         res.send(data);
-    }); 
+    });
 });
 
-app.get('/config', function(req, res) {
+app.get('/config', (req, res) => {
     fs.readFile(configFileName, (err, data) => {
-        if (err) throw err;             
+        if (err) throw err;
         res.json(JSON.parse(data));
-    });    
+    });
 });
 
-app.post('/config', function(req, res){
+app.post('/config', (req, res) => {
     var json = JSON.stringify(req.body);
-    fs.writeFile(configFileName, json, function (err) {
-        if (err) throw err;         
+    fs.writeFile(configFileName, json, err => {
+        if (err) throw err;
         console.log('Data written successfully!', json);
         res.end();
     });
 });
 
-app.listen(port, function(){
+app.listen(port, () => {
     console.log('Listening on port ' + port);
 })
